a839859526bef77c3835db2095b229ab097f7173
Viranchee <viranchee@outlook.com>
2020-02-22 17:53:00 +0530

    Add PushNotification project

---
+++ PushNotificationsDemo/PushNotificationsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PushNotificationsDemo
+//
+//  Created by Viranchee L on 22/02/20.
+//  Copyright © 2020 Viranchee L. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
